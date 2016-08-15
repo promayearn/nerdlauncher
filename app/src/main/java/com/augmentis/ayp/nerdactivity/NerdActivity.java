f4f741439716a6fe77a04e6e2b3cd68d18746ce3
Chayanit Kaensan <Chayanit.K@augmentis.biz>
2016-08-15 13:42:37 +0700

    init

---
+++ app/src/main/java/com/augmentis/ayp/nerdactivity/NerdActivity.java
@@ -0,0 +1,13 @@
+package com.augmentis.ayp.nerdactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NerdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nerd);
+    }
+}
