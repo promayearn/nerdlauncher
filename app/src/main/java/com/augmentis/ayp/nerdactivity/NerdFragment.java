f6681efc50db551b3e5a62af972a63fe871110c9
Chayanit Kaensan <Chayanit.K@augmentis.biz>
2016-08-15 15:36:48 +0700

    Change App name to Icon

--- app/src/main/java/com/augmentis/ayp/nerdactivity/NerdFragment.java
+++ app/src/main/java/com/augmentis/ayp/nerdactivity/NerdFragment.java
@@ -1,17 +1,26 @@
 package com.augmentis.ayp.nerdactivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -24,6 +33,8 @@ import java.util.List;
  * Created by Chayanit on 8/15/2016.
  */
 public class NerdFragment extends Fragment {
+
+    private static final String TAG = "NerdFragment";
 
     public static NerdFragment newInstance() {
 
@@ -43,7 +54,7 @@ public class NerdFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_nerd, container, false);
 
         mRecyclerView = (RecyclerView) v.findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         setupAdapter();
         mRecyclerView.setAdapter(new NerdAdapter(mActivities));
@@ -72,21 +83,54 @@ public class NerdFragment extends Fragment {
         mActivities = activities;
     }
 
-    class NerdViewHolder extends RecyclerView.ViewHolder {
+    class NerdViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
         private TextView mNameTextView;
+        private ImageView mImageView;
+        private ResolveInfo mResolveInfo;
 
         public NerdViewHolder(View itemView) {
             super(itemView);
 
-            mNameTextView = (TextView) itemView;
+            //mNameTextView = (TextView) itemView;
+            mImageView = (ImageView) itemView.findViewById(R.id.image_nerd);
+            mImageView.setOnClickListener(this);
+            mImageView.setOnLongClickListener(this);
+            //mNameTextView.setOnClickListener(this);
         }
 
         protected void bindActivity(ResolveInfo resolveInfo) {
             PackageManager pm = getActivity().getPackageManager();
             CharSequence activitiesName = resolveInfo.loadLabel(pm);
+            Drawable draw = resolveInfo.loadIcon(pm);
 
-            mNameTextView.setText(activitiesName);
+            mResolveInfo = resolveInfo;
+
+            //mNameTextView.setText(activitiesName);
+            mImageView.setImageDrawable(draw);
+        }
+
+        @Override
+        public void onClick(View view) {
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Intent i = new Intent(Intent.ACTION_MAIN);
+            i.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+
+            Log.i(TAG, "Launching " + activityInfo.applicationInfo.packageName + " ---> " + activityInfo.name);
+
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            PackageManager pm = getActivity().getPackageManager();
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Toast.makeText(getActivity(), activityInfo.loadLabel(pm), Toast.LENGTH_SHORT).show();
+
+            return true;
         }
     }
 
@@ -100,7 +144,7 @@ public class NerdFragment extends Fragment {
 
         @Override
         public NerdViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(getActivity()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            View v = LayoutInflater.from(getActivity()).inflate(R.layout.item_nerd, parent, false);
 
             return new NerdViewHolder(v);
         }
